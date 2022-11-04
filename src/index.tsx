49b650f8a2c64932dd681f67953eb4cddc125bf6
Juan Carlos <juanksoliveira@gmail.com>
2022-11-04 02:38:19 -0300

    refactor: move .jsx to .tsx files

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import routes from "./routes";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 const router = createBrowserRouter(routes);
 
